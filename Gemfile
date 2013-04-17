8aaaaa827065119b94f340da45e4a030b3f3b60d
chrisfitzpatrick <chrisfitzpatrick@chrisfitzpatricks-MacBook-Air.local>
2013-04-17 07:52:08 +0200

    adding capistrano

--- Gemfile
+++ Gemfile
@@ -20,9 +20,11 @@ group :assets do
 end
 
 gem 'capistrano'
+gem 'capistrano-ext'
+gem 'rvm-capistrano'
 gem 'knife-solo'
 gem 'knife-digital_ocean'
-
+gem 'unicorn'
 gem 'jquery-rails'
 gem "therubyracer"
 gem "less-rails" #Sprockets (what Rails 3.1 uses for its asset pipeline) supports LESS
@@ -35,17 +37,17 @@ gem 'delayed_job_mongoid'
 gem 'bibtex-ruby'
 gem 'citeproc-ruby'
 gem "haml-rails", ">= 0.4"
-gem "html2haml", ">= 1.0.1", :group => :development
+gem "html2haml", ">= 1.0.1", :group => [:development]
 gem "rspec-rails", ">= 2.12.2", :group => [:development, :test]
-gem "database_cleaner", ">= 1.0.0.RC1", :group => :test
-gem "mongoid-rspec", ">= 1.7.0", :group => :test
-gem "email_spec", ">= 1.4.0", :group => :test
+gem "database_cleaner", ">= 1.0.0.RC1", :group => [:test]
+gem "mongoid-rspec", ">= 1.7.0", :group => [:test]
+gem "email_spec", ">= 1.4.0", :group => [:test]
 gem "cucumber-rails", ">= 1.3.1", :group => :test, :require => false
-gem "launchy", ">= 2.2.0", :group => :test
-gem "capybara", ">= 2.0.3", :group => :test
+gem "launchy", ">= 2.2.0", :group => [:test]
+gem "capybara", ">= 2.0.3", :group => [ :test]
 gem "factory_girl_rails", ">= 4.2.0", :group => [:development, :test]
 gem "omniauth-mendeley", :git => "git://github.com/cfitz/omniauth-mendeley.git"
-gem "quiet_assets", ">= 1.0.2", :group => :development
+gem "quiet_assets", ">= 1.0.2", :group => [:development ]
 gem "figaro", ">= 0.6.3"
 gem "better_errors", ">= 0.7.2", :group => :development
 gem "binding_of_caller", ">= 0.7.1", :group => :development, :platforms => [:mri_19, :rbx]
---
+++ config/deploy.rb
@@ -0,0 +1,59 @@
+# config/deploy.rb
+
+require 'bundler/capistrano'
+
+
+set :rvm_ruby_string, :local               # use the same ruby as used locally for deployment
+set :rvm_autolibs_flag, "read-only"        # more info: rvm help autolibs
+
+before 'deploy:setup', 'rvm:install_rvm'   # install RVM
+before 'deploy:setup', 'rvm:install_ruby'  # install Ruby and create gemset, OR:
+before 'deploy:setup', 'rvm:create_gemset' # only create gemset
+
+require "rvm/capistrano"
+
+
+set :application, 'wmu-repo'
+
+set :stages, %w(production staging)
+set :default_stage, 'staging'
+require 'capistrano/ext/multistage'
+
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+
+set :repository, "https://github.com/cfitz/wmu-repo.git"
+set :deploy_to, "/var/www/#{application}"
+set :branch, 'development'
+
+set :scm, :git
+set :scm_verbose, true
+
+set :deploy_via, :remote_cache
+set :use_sudo, true
+set :keep_releases, 3
+set :user, 'chrisfitzpatrick'
+
+set :bundle_without, [:development, :test, :acceptance]
+set :rake, "#{rake} --trace"
+
+
+after 'deploy:update_code', :upload_env_vars
+
+after 'deploy:setup' do
+  sudo "chown -R #{user} #{deploy_to} && chmod -R g+s #{deploy_to}"
+end
+
+namespace :deploy do
+  desc <<-DESC
+  Send a USR2 to the unicorn process to restart for zero downtime deploys.
+  runit expects 2 to tell it to send the USR2 signal to the process.
+  DESC
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "sv 2 /home/#{user}/service/#{application}"
+  end
+end
+
+task :upload_env_vars do
+  #upload(".env.#{rails_env}", "#{release_path}/.env.#{rails_env}", :via => :scp)
+end
